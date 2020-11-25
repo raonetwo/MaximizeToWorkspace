6ffd3b8426d0ad7391d9c29bfe58886afbf3a880
Vinay Yadav <b6bdbc11@opayq.com>
2020-11-25 19:58:37 +0530

    Removing delay in moving newly created window as it is not required in 3.36

--- extension.js
+++ extension.js
@@ -1,13 +1,9 @@
 const Meta = imports.gi.Meta;
 const GLib = imports.gi.GLib;
 
-/* This has been tested with dynamic workspaces. It works well with dynamic workspaces, 
- * but can work well  with static if you have enough static ones.
- * Works well with external monitor as well, 
- * the idea being windows opened on external monitor are meant for just that monitor.
- * Any caveats that come with the way gnome handles multi monitor setups will remain
- * as we do not try to address  those here. However, we can open issues to track any nuances if found.
- * If there are any problems we can create an issue to track that.
+/* This has been tested with dynamic workspaces. It works well with dynamic workspaces, but can work well with static if you have enough static ones.
+ * Works well with external monitor as well, the idea being windows opened on external monitor are meant for just that monitor.
+ * Any caveats that come with the way gnome handles multi monitor setups will remain as we do not try to address those here.
  */
 
 /* Possible future options:
@@ -63,10 +59,10 @@ function check(win, change) {
     return;
   }
   // key name we will use to store in map here it is Id.
-  let name = win.get_id();
+  const name = win.get_id();
   // get list of other windows in the current workspace and the current display
   // TODO name this variable better.
-  let w = win.get_workspace().list_windows()
+  const w = win.get_workspace().list_windows()
     .filter(w => w !== win && !w.is_always_on_all_workspaces() && win.get_monitor() == w.get_monitor());
   // check if this method was called for a window that is not maximized
   // TODO this should be moved out into a separate function and unmaximized signal should be process with that function
@@ -113,37 +109,7 @@ function check(win, change) {
   }
 }
 
-// This function is for handling newly created windows that start maximized TODO: name better
-// The better approach would have been only to trigger the check function when the window is "ready".
-// I have faced a lot of rendering issues while working with this, likely because of my lack of knowledge of gnome.
-// and more so by the lack of documentation in the JS API, 
-// since I did not bother reading the long prose in other pieces of documentation which looked incomplete anyways.
-// e.g the window got maximized and moved to a new workspace but was not rendered in the desktop/workspace, 
-// so you are left staring at your desktop background but your keyboard and mouse are interacting with the app.
-// it showed up correctly in preview in the dock and application overview (when you press super) but not on the desktop
-// back then I had to minimize and unminimize the window to get it to render. story from gnome 3.34 
-function checkFullScreen(win) {
-  // stuff from trial and error lies below, this works but I didn't bother cleaning it up and check what is required and what is not
-  // basically we are checking if window is ready to be put to new workspace before trying to do just that
-  if (!win || !win.get_workspace()) {
-    return false;
-  }
-  // I should read the documentation for this to add a meaningful comment
-  // basically we are checking if its "visible" and "allocated" need to read the doc to understand what it means
-  // as I said, stuff from trial and error. HEHE
-  var windowActor = win.get_compositor_private();
-  if (!windowActor || !windowActor.visible || !windowActor.has_allocation()) {
-    return false;
-  }
-  // Check if this is a maximized window and it is indeed a new window and we haven't seen it before and it has "focus" whatever that means. TODO read the docs if they exist
-  if (win.get_maximized() === Meta.MaximizeFlags.BOTH &&
-    _old_workspaces[win.get_id()] === undefined &&
-    win.has_focus()) {
-    check(win, Meta.SizeChange.MAXIMIZE);
-  }
-  return false;
-}
-
+// Does as the name suggests, brings you back to the workspace you started in.
 function handleWindowClose(act) {
   let win = act.meta_window;
   let name = win.get_id();
@@ -154,31 +120,17 @@ function handleWindowClose(act) {
 
 // need to understand "handles", these are just object arrays to store the "handles" that connect with signals
 const _window_manager_handles = [];
-const _display_handles = [];
 
 // Runs when the extension is enabled, basically connects to the signals and save the handles
 // whenever the signal is emitted our connected handles, process the signal.
-// Again need to check the documentation on how this works, this was part of the relic code, this project is "inspired" by
 function enable() {
-  // add the window created event handler to the array.
-  _display_handles.push(global.display.connect('window-created', (_, win) => {
-    // There are some wierd windows, trying to move which result in gnome shell crash as they get closed while we are trying to move them
-    // found this check through trial and error as I didn't read the doc (someone send me the link already)
-    if (win.get_layer() !== Meta.StackLayer.NORMAL) {
-      return;
+  // removing delay and using map again
+  _window_manager_handles.push(global.window_manager.connect('map', (_, act, change) => {
+    if (act.meta_window.get_maximized() === Meta.MaximizeFlags.BOTH) {
+      check(act.meta_window, change);
     }
-    // Another check that arose from trial and error, if someone wants to check their similar extension they should try opening Unity Game Editor on ubuntu
-    // and open a new firefox private window.
-    // Need to ensure that the frameless window does not need to be moved as most of such windows are transient
-    if (win.get_frame_type() !== Meta.FrameType.NORMAL && win.get_frame_type() !== Meta.FrameType.BORDER) {
-      return;
-    }
-    // This is required to add a delay before we try to move any windows om window-created event
-    GLib.timeout_add(GLib.PRIORITY_LOW, 300, checkFullScreen.bind(this, win));
   }));
-  // Add size-change event handler for windows that are already created. check if the change is of type Maximize or Unmaximize
-  // Gnome really need to work on their documentation or googe need to work on their search.
-  // Once both of them are done, I will work on my comprehension skills and on finding out it I have ADHD.
+  // Add size-change event handler for windows that are already created.
   _window_manager_handles.push(global.window_manager.connect('size-change', (_, act, change) => {
     check(act.meta_window, change);
   }));
@@ -194,5 +146,4 @@ function disable() {
 
   // we just disconnet the handlers for the events.
   _window_manager_handles.splice(0).forEach(h => global.window_manager.disconnect(h));
-  _display_handles.splice(0).forEach(h => global.display.disconnect(h));
 }
